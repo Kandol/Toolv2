ea72af3bce38bf7ba9669f6a59f5ea0f2252ac2c
Kandol <46228985+Kandol@users.noreply.github.com>
2019-08-09 09:55:10 +0700

    Add files via upload

---
+++ v2.sh
@@ -0,0 +1,335 @@
+#=====================Root@Kandol=================#
+# _  __           _   _ ____     ____   _         #
+#| |/ /    /\    | \ | |  __ \ /  __ \ | |        #
+#| ' /    /  \   |  \| | | |  \| | |  || |        #
+#|  <    / /\ \  | . ` | | |  || | |  || |        #
+#|  .\  / /__\ \ | | \ | |_|  || |_|  || |___     #
+#|_|\_\/_/    \_\|_| \_|_____/ \_____/ |_____|    #
+#####################################
+#MAU NGAPAIN AJG?
+#MAU DIRECODE?
+#GW BIKIN TOOLS INI CAPEK HARGAI DONG KARYA ORANG
+#KLO MAU SHARE TOOLS SEDERHANA INI KE TEMEN LU
+#ANDA KAN NGERTI PYTHON PYTHON2 PYTHON3
+#JADI KNPA GK BIKIN SENDIRI AJA?
+
+clear
+bi='\033[34;1m' #BIRU
+i='\033[32;1m' #IJO
+pur='\033[35;1m' #PURPLE
+cy='\033[36;1m' #CYAN
+me='\033[31;1m' #MERAH
+pu='\033[37;1m' #PUTIH
+ku='\033[33;1m' #KUNING
+echo
+echo
+echo
+clear
+echo
+echo $red" Tool akan di install dalam waktu 5 detik saja"
+echo $red" TUNGGU AJG"   
+sleep 1
+echo '''\a
+\033[31;1m               _
+\033[31;1m              / |
+\033[31;1m              | |
+\033[31;1m              | |
+\033[31;1m             _|_|_ '''
+sleep 1
+apt update && apt upgrade
+apt install nano figlet curl
+clear
+echo
+echo
+echo $red" Tool akan di install dalam waktu 5 detik saja"
+echo $red" TUNGGU AJG"
+sleep 1
+echo '''\a
+\033[37;1m              ____
+\033[37;1m             |___ \
+\033[37;1m               __) |
+\033[37;1m              / __/
+\033[37;1m             |_____| '''
+sleep 1
+pip2 install mechanize
+pip2 install lolcat
+pip2 install requests
+clear
+echo
+echo
+echo $red" Tool akan di install dalam waktu 5 detik saja"
+echo $red" TUNGGU AJG"
+sleep 1
+echo '''\a
+\033[31;1m             _____
+\033[31;1m            |___ /
+\033[31;1m              |_ \
+\033[31;1m             ___) |
+\033[31;1m            |____/ '''
+sleep 1
+clear
+echo
+echo
+echo $red" Tool akan di install dalam waktu 5 detik saja"
+echo $red" TUNGGU AJG"
+sleep 1
+echo '''\a
+\033[33;1m             _  _
+\033[33;1m            | || |
+\033[33;1m            | || |_
+\033[33;1m            |__   _|
+\033[33;1m               |_| '''
+sleep 1
+clear
+echo
+echo
+echo $red" Tool akan di install dalam waktu 5 detik saja"
+echo $red" TUNGGU AJG"
+sleep 1
+echo '''\a
+\033[32;1m              ____
+\033[32;1m             | ___|
+\033[32;1m             |___ \
+\033[32;1m              ___) |
+\033[32;1m             |____/ '''
+sleep 1
+clear
+echo
+clear
+echo "Loading .."
+load
+clear
+echo Selamat datang nyet, Siapa nick ente? #tulisan keluar
+read nick #membaca yang ditulis
+clear
+echo Selamat datang $nick ":)" SCRIPT KIDDIES! :v
+echo $cy"===================ROOT@"$pur"KANDOL ==================="
+echo
+echo $me"█"$bi"  Author  : ROOT@KANDOL       "$me
+echo $ku"█"$bi"  WhatsApp: 082130505651"$ku 
+echo $bi"█"$bi"  Team: ALI ATTACKER - INDOJECT SQUAD - INDONESIAN FIGHTER CYBER - DARK 3XPLOIT CYBER - INDONESIAN TERMUX ASOCIATION"$bi
+echo $i"█"$bi"  Youtube : ALI ATTACKER TEAM"$i
+echo $i"█"$bi"   Blogspot : Https://www.Aliattacker.xyz"
+echo
+echo $me"===================ROOT@"$ku"KANDOL ==================="
+echo
+echo $i">>>>>>>>> Menu"
+echo $me"⟨"$ku"1"$i"⟩"bii" -» Report FB"
+echo $me"⟨"$ku"2"$i"⟩"$bi" -» Hack Fb (Dark-Fb)"
+echo $me"⟨"$ku"3"$i"⟩"$bi" -» Brute Force Facebook Cracker"
+echo $me"⟨"$ku"4"$i"⟩"$bi" -» Mbf"
+echo $me"⟨"$ku"5"$i"⟩"$bi" -» Cloning Yahoo"
+echo $me"⟨"$ku"6"$i"⟩"$bi" -» OsiF"
+echo $me"⟨"$ku"7"$i"⟩"$bi" -» Bot Reaction Facebook"
+echo $me"⟨"$ku"8"$i"⟩"$bi" -» Profileguard Facebook"
+echo $me"⟨"$ku"9"$i"⟩"$bi" -» Spam Sms Unlimited!"
+echo $me"⟨"$ku"10"$i"⟩"$me"-» Spam Call [FREE]"
+echo $me"⟨"$ku"11"$i"⟩"$me"-» Sadap Whatsapp"
+echo $me"⟨"$ku"12"$i"⟩"$me"-» Youtube-dl"
+echo $me"⟨"$ku"13"$i"⟩"$me"-» Tools Trojan Perusak"
+echo $me"⟨"$ku"14"$i"⟩"$pu"-» DDOS TERBARU"
+echo $me"⟨"$ku"15"$i"⟩"$pu"-» SPAM GMAIL"
+echo $me"⟨"$ku"16"$i"⟩"$pu"-» TOOLS BUCIN (Semoga Selamat)"
+echo $me"⟨"$ku"17"$i"⟩"$pu"-» Keluar"
+echo
+echo $pu"╭─["$i"Masukan Nomer"$pu"]"
+read -p"╰─> : " pil
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "KANDOL ."|lolcat
+sleep 1
+git clone https://github.com/IlayTamvan/Report.git
+cd Report
+unzip Report.zip
+python2 Report.py
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "KANDOL ."|lolcat
+sleep 1
+git clone https://github.com/SantriCyber/santri-IDN
+cd santri-IDN
+python2 santri-IDN.py
+fi
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "KANDOL ."|lolcat
+sleep 1
+git clone https://github.com/Edi-nasa47/7.git
+cd 7
+python2 crack.py
+fi
+
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "KANDOL ."|lolcat
+sleep 1
+git clone https://github.com/Edi-nasa47/1.git
+cd 1
+python2 MBF.py
+fi
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "KANDOL ."|lolcat
+sleep 1
+git clone https://github.com/Edi-nasa47/2.git
+cd 2
+pip2 install requests mechanize
+pip2 install requirements
+python2 boss.py
+fi
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "KANDOL ."|lolcat
+sleep 1
+git clone https://github.com/Edi-nasa47/8.git
+cd 8
+pip2 install -r requirements.txt
+python2 osif.py
+fi
+
+if [ $pil = 7 ]
+then
+clear
+figlet -f slant "KANDOL ."|lolcat
+sleep 1
+git clone https://github.com/Edi-nasa47/5.git
+cd 5
+python2 robot.py
+fi
+
+if [ $pil = 8 ]
+then
+clear
+figlet -f slant "KANDOL ."|lolcat
+sleep 1
+git clone https://github.com/Edi-nasa47/13.git
+cd 13
+php profile.php
+fi
+
+if [ $pil = 9 ]
+then
+clear
+figlet -f slant "KANDOL ."|lolcat
+sleep 1
+git clone https://github.com/Muhamad-Ropik/SpamSmsUnlimited
+cd SpamSmsUnlimited
+php Spam-Pik.php
+fi
+
+if [ $pil = 10 ]
+then
+clear
+figlet -f slant "KANDOL ."|lolcat
+sleep 1
+git clone https://github.com/Kandol/CallSpam
+cd CallSpam
+php SpamCall.php
+fi
+
+if [ $pil = 11 ]
+then
+clear
+figlet -f slant "KANDOL ."|lolcat
+sleep 1
+git clone https://github.com/Muhamad-Ropik/WhatsRop
+cd WhatsRop
+sh WhatsRop.sh
+fi
+
+if [ $pil = 12 ]
+then
+clear
+figlet -f slant "KANDOL ."|lolcat
+sleep 1
+git clone https://github.com/Edi-nasa47/6.git
+cd 6
+sh youtube-dl.sh
+fi
+
+if [ $pil = 13 ]
+then
+clear
+figlet -f slant "KANDOL ."|lolcat
+sleep 1
+git clone https://github.com/justahackers/perusak
+cd perusak
+python2 perusak.py
+fi
+
+if [ $pil = 14 ]
+then
+clear
+figlet -f slant "KANDOL ."|lolcat
+sleep 1
+git clone https://github.com/Muhamad-Ropik/DDOSnew
+cd DDOSnew
+python2 DdosPik.py
+fi
+
+if [ $pil = 15 ]
+then
+clear
+figlet -f slant "KANDOL ."|lolcat
+sleep 1
+git clone https://github.com/Muhamad-Ropik/DDOSnew
+cd Spam-Gmail
+php Spam-Pik.php
+fi
+
+if [ $pil = 16 ]
+then
+clear
+figlet -f slant "KANDOL ."|lolcat
+sleep 1
+git clone https://github.com/Muhamad-Ropik/Bucin-Tools
+cd Bucin-Tools
+python2 Tools-Bucin.py
+fi
+
+if [ $pil = 17 ]
+then
+clear
+figlet -f slant "E X I T"|Ulolcat
+sleep 2
+echo $i"Good Bay MEMEK..."
+echo $i"Terima Kasih sudah mengunakan Tools ini"
+sleep 2
+echo $i"Silahkan di pakai semoga bermanfaat"
+sleep 2
+echo $i"Buat Hacker Hacker Club"
+sleep 2
+echo $i"Kalo Gak bisa Makenya Kontak saya "
+echo $i"Melalui Via:"
+sleep 2
+echo $ku"Whatsapp :"$pur" 082130505651"
+echo $ku"YouTube  :"$pur" https://www.youtube.com/channel/UCHy_X19au3eyrzOPVMPFs4g"
+sleep 2
+echo $i"dan jangan lupa "$cy" LIKE SHARE AND SUBSCRIBE"
+sleep 2
+echo $i"Video Channel "$cy" ALI ATTACKER TEAM"
+sleep 2
+echo $i"Agar tambah semangat Upload Video²"
+sleep 2
+echo $i"terbaru menarik dari saya"
+echo $i"silahkan Aktifkan juga ya Budjank Tombol Loncengnya"
+sleep 2
+echo $i"Agar dapat pemberitahuan Video²"
+echo $i"selanjutnya dari saya OKE"
+sleep 2
+echo
+echo $cy"Thanks FOR LIKE AND SUBCRIBE...!!!"
+exit
+fi
